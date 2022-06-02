b841efa8bda94cca0615d5b1719e293841e86738
Violetta Ryabtsev <wondervioletta@gmail.com>
2022-06-01 17:53:47 -0700

    add nested routes to App components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,30 @@
-
+import { Routes, Route, Outlet} from "react-router-dom"
 import "./category.style.scss"
-import Categories from "./components/categories/categories";
+import Home from "./routes/home/HomeComponent"
+
+const Navigation =()=>{
+return (
+<div>
+  <div>
+    <h1>im a naviagation bar</h1>
+  </div>
+  <Outlet/>
+</div>
+   );
+};
+
+const Shop=()=>{
+return  <h2>shop here</h2>;
+};
+
 const App = () => {
-
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-
   return (
-    <div >
-     <Categories categories={categories}/>
-    </div>
+    <Routes> 
+      <Route path="/" element={<Navigation/>} >
+         <Route index element={<Home/>} /> 
+           <Route path="shop" element={<Shop/>}></Route>
+      </Route> 
+    </Routes>
   );
 }
 
